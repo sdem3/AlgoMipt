848660368fcd2c26ff564fb715e5ce033ed61c3f
sdem3 <108931159+sdem3@users.noreply.github.com>
2022-12-19 11:40:04 +0300

    Update task-D.cpp

--- task-D.cpp
+++ task-D.cpp
@@ -4,9 +4,9 @@
 #include <vector>
 const int64_t kLongTwo = 1 << 30;
 class MaxHeap {
- public:
+public:
   MaxHeap() : data(1), size(0) {}
-  void Insert(long long val) {
+  void Insert(int64_t val) {
     if (size == data.size()) {
       data.resize(2 * size);
     }
@@ -14,7 +14,7 @@ class MaxHeap {
     SiftUp(size - 1);
   }
   bool IsEmpty() { return size == 0; }
-  long long GetMax() { return data[0]; }
+  int64_t GetMax() { return data[0]; }
   void ExtractMax() {
     if (size > 0) {
       std::swap(data[0], data[size - 1]);
@@ -22,7 +22,7 @@ class MaxHeap {
       SiftDown(0);
     }
   }
-  std::vector<long long> data;
+  std::vector<int64_t> data;
   size_t size;
   void SiftUp(size_t index) {
     if (index > 0) {
@@ -49,41 +49,55 @@ class MaxHeap {
     }
   }
 };
-void Heapify(MaxHeap& hp, int64_t* request) {
-  hp.data[0] = (request[2] * request[3] + request[4]) % kLongTwo;
-  if (request[1] >= request[0]) {
-    request[1] = request[0];
+
+struct Request {
+public:
+  size_t n;
+  size_t k;
+  int64_t a0;
+  int64_t x;
+  int64_t y;
+};
+
+void Heapify(MaxHeap& hp, Request re) {
+  hp.data[0] = (re.a0* re.x + re.y) % kLongTwo;
+  if (re.k >= re.n) {
+    re.k = re.n;
   }
-  for (int i = 1; i < request[1]; i++) {
-    hp.data.push_back((hp.data[i - 1] * request[3] + request[4]) % kLongTwo);
+  for (int i = 1; i < re.k; i++) {
+    hp.data.push_back((hp.data[i - 1] * re.x + re.y) % kLongTwo);
   }
-  hp.size = request[1];
-  request[2] = hp.data[request[1] - 1];
-  for (int i = hp.size / 2; i >= 0; i--) {
+  hp.size = re.k;
+  re.a0 = hp.data[re.k - 1];
+  for (size_t i = hp.size / 2; i >= 0; i--) {
     hp.SiftDown(i);
   }
   int64_t temp;
-  for (int i = request[1]; i < request[0]; i++) {
-    temp = (request[2] * request[3] + request[4]) % kLongTwo;
+  for (size_t i = re.k; i < re.n; i++) {
+    temp = (re.a0 * re.x + re.y) % kLongTwo;
     if (temp < hp.GetMax()) {
       hp.ExtractMax();
       hp.Insert(temp);
     }
-    request[2] = temp;
+    re.a0 = temp;
   }
 }
+
 int main() {
+  Request re;
+  std:: cin>> re.n >> re.k;
+  std::cin >> re.a0 >> re.x >> re.y;
   int64_t* request = new int64_t[5];
   std::cin >> request[0] >> request[1];
   std::cin >> request[2] >> request[3] >> request[4];
   MaxHeap hp;
   std::vector<int64_t> res;
-  Heapify(hp, request);
+  Heapify(hp, re);
   for (int i = 0; i < request[1]; i++) {
     res.push_back(hp.GetMax());
     hp.ExtractMax();
   }
-  for (int i = request[1] - 1; i > -1; i--) {
+  for (size_t i = re.k - 1; i > -1; i--) {
     std::cout << res[i] << ' ';
   }
 }
